31bcb0ef7a384cfdd639acb554934367bccfcf54
Manuelpv17 <manuelpv17@outlook.com>
2021-02-15 22:48:22 -0500

    Add Closure task

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,13 @@
+//Closure
+
+function welcomeMessage(fullName) {
+  alert(`Welcome ${fullName}`);
+}
+
+const guillaume = welcomeMessage("Guillaume");
+const alex = welcomeMessage("Alex");
+const fred = welcomeMessage("Fred");
+
+guillaume();
+alex();
+fred();
