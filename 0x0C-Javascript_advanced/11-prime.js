214b5935ce7ba643f66e5e1dc18b28cd7c713a6b
Manuelpv17 <manuelpv17@outlook.com>
2021-02-23 23:56:33 -0500

    Add count prime number x 100, setTimeout

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,27 @@
+const { performance } = require("perf_hooks");
+
+const t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+  setTimeout(() => countPrimeNumbers(), 0);
+}
+const t1 = performance.now();
+console.log(
+  `Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`
+);
+
+function countPrimeNumbers() {
+  prime_numbers = [2];
+  for (let num = 3; num < 100; num += 2) {
+    flag = 0;
+    for (let j = 0; j < prime_numbers.length; j++) {
+      if (num % prime_numbers[j] === 0) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag === 0) {
+      prime_numbers.push(num);
+    }
+  }
+  return prime_numbers.length;
+}
